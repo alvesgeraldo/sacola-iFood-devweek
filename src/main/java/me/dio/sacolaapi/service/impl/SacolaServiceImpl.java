12a5b5f6a60777e67671055c483035d24183acc6
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-10-03 09:21:45 -0300

    Iniciando Sacola Service Impl

---
+++ src/main/java/me/dio/sacolaapi/service/impl/SacolaServiceImpl.java
@@ -0,0 +1,28 @@
+package me.dio.sacolaapi.service.impl;
+
+import me.dio.sacolaapi.model.Item;
+import me.dio.sacolaapi.model.Sacola;
+import me.dio.sacolaapi.resource.dto.ItemDto;
+import me.dio.sacolaapi.service.SacolaService;
+
+public class SacolaServiceImpl implements SacolaService {
+
+  @Override
+  public Sacola fecharSacola(Long id, int formaPagamento) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Item incluirItemSacola(ItemDto itemDto) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Sacola verSacola(Long id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
