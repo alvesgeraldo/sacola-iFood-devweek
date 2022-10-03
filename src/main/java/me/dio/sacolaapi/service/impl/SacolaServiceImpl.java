f8d5ea8ee7355d5b27029f6a6e744d73d330dcce
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-10-03 09:28:34 -0300

    Implementado método ver sacola

--- src/main/java/me/dio/sacolaapi/service/impl/SacolaServiceImpl.java
+++ src/main/java/me/dio/sacolaapi/service/impl/SacolaServiceImpl.java
@@ -1,28 +1,36 @@
 package me.dio.sacolaapi.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import me.dio.sacolaapi.model.Item;
 import me.dio.sacolaapi.model.Sacola;
+import me.dio.sacolaapi.repository.SacolaRepository;
 import me.dio.sacolaapi.resource.dto.ItemDto;
 import me.dio.sacolaapi.service.SacolaService;
 
+@RequiredArgsConstructor
 public class SacolaServiceImpl implements SacolaService {
+
+  private final SacolaRepository sacolaRepository;
 
   @Override
   public Sacola fecharSacola(Long id, int formaPagamento) {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public Item incluirItemSacola(ItemDto itemDto) {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public Sacola verSacola(Long id) {
-    // TODO Auto-generated method stub
-    return null;
+    
+    return sacolaRepository.findById(id).orElseThrow(
+      () -> {
+        throw new RuntimeException("Essa sacola não existe!");
+      }
+    );
+    
   }
   
 }
